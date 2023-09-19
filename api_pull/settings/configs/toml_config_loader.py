24a63219161588179bbd9dc80c8a83504196bf5e
austisnyder <rsnyder.austin@gmail.com>
2023-09-19 16:28:24 -0500

    Restructured toml_config_loader.py to be more readable.

--- api_pull/settings/configs/toml_config_loader.py
+++ api_pull/settings/configs/toml_config_loader.py
@@ -13,20 +13,15 @@ class MissingConfigFileValueError(Exception):
 
 
 class TomlConfigLoader:
-    """
-        If logs_file_path is not provided, the program will access 'settings.toml' within the file folder.
-    """
     _instance = None
     _toml_config_data = None
 
     @staticmethod
-    def _generate_config_file_path():
-        parent_package_path = os.path.dirname(__file__)
-        config_file_path = os.path.join(parent_package_path, 'config.toml')
-        return config_file_path
-
-    @staticmethod
-    def _get_config_data(file_path):
+    def _get_all_config_data(file_path) -> dict:
+        """
+        Raises MissingConfigFileError if no file is found at the given file path.
+        :return: A dict of all config file data from the given file path.
+        """
         try:
             with open(file_path, "rb") as toml_file:
                 return tomli.load(toml_file)
@@ -38,13 +33,23 @@ class TomlConfigLoader:
             logging.error(err_msg)
             raise MissingConfigFileError(err_msg)
 
+    @staticmethod
+    def _generate_config_file_path() -> str:
+        parent_package_path = os.path.dirname(__file__)
+        config_file_path = os.path.join(parent_package_path, 'config.toml')
+        return config_file_path
+
     def __new__(cls, config_file_path=None):
+        
+        """
+            If logs_file_path is not provided, the program will attempt to access 'config.toml' within the current package.
+        """
         if cls._instance is None:
             cls._instance = super(TomlConfigLoader, cls).__new__(cls)
             if not config_file_path:
-                config_file_path = cls._generate_config_file_path()
+                config_file_path = TomlConfigLoader._generate_config_file_path()
 
-            cls._toml_config_data = cls._get_config_data(config_file_path)
+            cls._toml_config_data = cls._get_all_config_data(config_file_path)
         return cls._instance
 
     @staticmethod
@@ -53,21 +58,14 @@ class TomlConfigLoader:
         TomlConfigLoader._toml_config_data = None
 
     @staticmethod
-    def get_config_data(configs_file_path=None):
-        if not TomlConfigLoader._instance:
-            TomlConfigLoader(configs_file_path)
-
-        return TomlConfigLoader._toml_config_data
-
-    @staticmethod
     def get_config(section, config_name, configs_file_path=None):
         if not TomlConfigLoader._instance:
             TomlConfigLoader(configs_file_path)
-
-        configs = TomlConfigLoader.get_config_data()
+        
+        config_data = TomlConfigLoader._toml_config_data
 
         try:
-            config = configs[section][config_name]
+            config = config_data[section][config_name]
         except KeyError:
             err_msg = MsgLoader.get_log(section='config_loader',
                                         log_name='missing_config_variable',
